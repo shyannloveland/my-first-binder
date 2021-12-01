c2347fd40a2043a9a54d7ad3fa8e66c98017fc3b
shyannloveland <86621348+shyannloveland@users.noreply.github.com>
2021-12-01 14:44:00 -0700

    Update loops.py

--- loops.py
+++ loops.py
@@ -10,3 +10,16 @@ i = 1
 while i < 6:
   print(i)
   i += 1
+
+  
+#Nested Loops
+i = 2
+while(i < 100):
+  j = 2
+  while(j <= (i/j):
+        if not(i%j): break
+        j = j + 1
+  if(j > i/j): print i, " is prime"
+  i = i+1
+        
+print "Good bye!"
